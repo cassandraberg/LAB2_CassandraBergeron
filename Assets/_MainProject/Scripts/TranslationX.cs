65739a7ded426ba4762a101da22716f11656d2f0
cassandraberg <104954839+cassandraberg@users.noreply.github.com>
2023-03-22 15:32:16 -0400

    Script translation

---
+++ Assets/_MainProject/Scripts/TranslationX.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslationX : MonoBehaviour
+{    //Attributs
+    [SerializeField] private float _positionXDepart = 1.016f;
+    [SerializeField] private float _positionXFin = 1.98f;
+    [SerializeField] private float _positionY = 3.21499f;
+    [SerializeField] private float _positionZ = 0.36999f;
+    [SerializeField] private float _speed = 1;
+    private float _positionBetweenPoints = 0;
+
+    private float _direction = 1;
+
+    //méthodes privées
+    private void FixedUpdate()
+    {
+        Vector3 _positionInitiale = new Vector3(_positionXDepart, _positionY, _positionZ);
+        Vector3 _positionFinale = new Vector3(_positionXFin, _positionY, _positionZ);
+
+        transform.position = Vector3.Lerp(_positionInitiale, _positionFinale, _positionBetweenPoints);
+        _positionBetweenPoints += Time.deltaTime * _speed * _direction;
+        if (_positionBetweenPoints >= 1 || _positionBetweenPoints <= 0)
+            _direction *= -1;
+        return;
+    }
+}
